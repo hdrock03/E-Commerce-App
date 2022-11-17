6464da534a612c1eb949812dd66a24b72d9ab3f2
hdrock03 <hardik.1rn16ec036@gmail.com>
2022-11-17 14:53:34 +0530

    cart page done

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Cart from "./pages/Cart";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Product from "./pages/Product";
@@ -11,7 +12,8 @@ function App() {
       {/* <ProductList /> */}
       {/* <Product /> */}
       {/* <Register /> */}
-      <Login />
+      {/* <Login /> */}
+      <Cart />
     </div>
   );
 }
